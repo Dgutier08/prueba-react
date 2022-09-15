96a3c8beaaa1c8f59709c5fd5548851b37a3b6e2
Dgutier08 <102337196+Dgutier08@users.noreply.github.com>
2022-09-15 16:22:59 -0300

    Update MiApi.jsx

--- prueba/src/components/MiApi.jsx
+++ prueba/src/components/MiApi.jsx
@@ -1,67 +1,63 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-function MiApi(){
-    const [actores, SetListadoActores] = useState ([]);
-    const [search, setSearch] = useState ("");
-    const [actoresdata, actoresSetData] = useState (""); 
-    useEffect (() =>{
-        const obtenerPersonaje = async ()=>{
-            const url = 'http://thronesapi.com/api/v2/Characters';
-            const result = await axios.get(url);
-            const {data}  = await result;
-            console.log(result.data);
-            
-           data.sort((a, b) => {
-              let x = a.name;
-              let y = b.name;
-              if (x < y) {
-                return -1;
-              } else {
-                return 1;
-              }
-            });
-            SetListadoActores(data);
+function MiApi() {
+  const [actores, setListadoActores] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const obtenerPersonaje = async () => {
+      const url = "http://thronesapi.com/api/v2/Characters";
+      const result = await axios.get(url);
+      const { data } = result;
+
+      const filtrados = data.sort((a, b) => {
+        let x = a.firstName;
+        let y = b.firstName;
+        if (x < y) {
+          return -1;
+        } else {
+          return 1;
         }
-        obtenerPersonaje()
-    },[]);
+      });
+      setListadoActores(filtrados);
+    };
+    obtenerPersonaje();
+  }, []);
 
-    const searchHandler = (e) => {
-      setSearch(e.target.value);
-    }
-    console.log(search);
-    const lista = !search
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const lista = !search
     ? actores
-    : actores.filter((x) => x.name.toLowerCase().includes(setSearch))
+    : actores.filter((x) =>
+        x.firstName.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+      );
 
-    return (
+  return (
     <div>
-<input 
-value={search}
-onChange={(e)=>searchHandler (e)}
-type="text"
-placeholder="buscardor"
-ClassName=""
-/>
-<h1>Actores de Game Of Throne</h1>
- <ul>
-  {actores.length === 0 && <p>Cargando...</p>}
-   {actores.map((personas,i)=> {
-     return (  
+      <input
+        value={search}
+        onChange={(e) => searchHandler(e)}
+        type="text"
+        placeholder="buscardor"
+      />
+      <h1>Actores de Game Of Throne</h1>
+      <ul>
+        {lista.length === 0 && <p>Cargando...</p>}
+        {lista.map((personas, i) => {
+          return (
             <li key={i}>
-                <h4>Nombre: {personas.fullName}</h4> 
-                <img src={personas.imageUrl} alt = "imagenes" width = '200'/>
-                <h5>{personas.family}</h5>
+              <h4>Nombre: {personas.fullName}</h4>
+              <img src={personas.imageUrl} alt="imagenes" width="200" />
+              <h5>{personas.family}</h5>
             </li>
-          )
-
-        }) 
-    }    
-    
-   </ul>
-
-  </div>
-    )
-}   
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
 
 export default MiApi;
